abcd4771a830eac83ede658ac161251fa4f7aa1c
vkr1162 <97697727+vkr1162@users.noreply.github.com>
2025-01-02 01:59:51 -0600

    Time: 1 ms (97.41%), Space: 41.3 MB (77.31%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,27 +1,29 @@
 class Solution {
     public int maxScore(String s) {
-        int max = 0;
-        
-        for(int i=1;i<s.length();i++){
-            String s1 = s.substring(0,i);
-            String s2 = s.substring(i, s.length());
-            int c1 = 0;
-            for(int j=0;j<s1.length();j++){
-                if(s1.charAt(j)=='0'){
-                    c1++;
-                }
-            }
-            int c2 = 0;
-            for(int j=0;j<s2.length();j++){
-                if(s2.charAt(j)=='1'){
-                    c2++;
-                }
-            }
-            int res = c1 + c2;
-            if(max<res){
-                max = res;
+      int oneCount = 0;
+      int zeroCount = 0;
+      int max = 0;
+      for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='1'){
+            oneCount++;
+        }
+      }
+      for(int i=0;i<s.length()-1;i++){
+        if(s.charAt(i)=='0'){
+            zeroCount++;
+            int x = oneCount + zeroCount;
+            if(max<x){
+                max = x;
             }
         }
-        return max;
+        else if(s.charAt(i)=='1'){
+            oneCount--;
+            int x = oneCount + zeroCount;
+            if(max<x){
+                max = x;
+            }
+        }
+      }
+    return max;
     }
 }
